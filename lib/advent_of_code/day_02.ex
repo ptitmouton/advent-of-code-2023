89825587a9e856dfa0ac3f1fe571d532dc13658e
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2023-12-02 10:40:07 +0100

    day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,75 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  @compare %{
+    red: 12,
+    green: 13,
+    blue: 14
+  }
+
+  defmodule Reveal do
+    defstruct red: 0, green: 0, blue: 0
+
+    def from_description(description_string) do
+      Regex.scan(~r/((\d+) (blue|red|green))/U, description_string)
+      |> Enum.reduce(%Reveal{}, fn [_, _, count, color], acc ->
+        {count, ""} = Integer.parse(count)
+
+        Map.put(acc, String.to_atom(color), count)
+      end)
+    end
+
+    def merge_greatest_values(%__MODULE__{} = base, %__MODULE{} = other) do
+      Map.merge(base, other, fn _keyk, v1, v2 ->
+        max(v1, v2)
+      end)
+    end
+
+    def product(%__MODULE{red: r, green: g, blue: b}), do: r * g * b
   end
 
-  def part2(_args) do
+  def part1(input) do
+    input
+    |> get_games()
+    |> Enum.filter(fn {_, reveals} ->
+      Enum.all?(reveals, fn reveal ->
+        reveal.red <= @compare.red &&
+          reveal.green <= @compare.green &&
+          reveal.blue <= @compare.blue
+      end)
+    end)
+    |> Enum.reduce(0, fn {gameno, _}, acc ->
+      acc + gameno
+    end)
+  end
+
+  def part2(input) do
+    input
+    |> get_games()
+    |> Enum.map(fn {_, reveals} ->
+      reveals
+      |> Enum.reduce(%Reveal{}, &Reveal.merge_greatest_values(&2, &1))
+    end)
+    |> Enum.map(&Reveal.product/1)
+    |> Enum.reduce(&(&1 + &2))
+  end
+
+  defp get_games(input) do
+    input
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(fn line ->
+      [description, reveal_list] = String.split(line, ":")
+
+      {game_number, ""} =
+        description
+        |> String.replace(~r/[^\d+]/, "")
+        |> Integer.parse()
+
+      reveals =
+        reveal_list
+        |> String.split(";")
+        |> Enum.map(&Reveal.from_description/1)
+
+      {game_number, reveals}
+    end)
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,27 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """
 
-    assert result
+    8 = part1(input)
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    input = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """
 
-    assert result
+    2286 = part2(input)
   end
 end
