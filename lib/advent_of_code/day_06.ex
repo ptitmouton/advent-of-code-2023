d3bab08a7cb7366ff8044b2a763c15dd8499f2b2
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2023-12-06 07:44:42 +0100

    WIP: day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,79 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse_races()
+    |> Enum.map(fn {time, distance} ->
+        get_possible_distances(time)
+        |> Enum.filter(&(&1 > distance))
+        |> length()
+    end)
+    |> Enum.product()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    {time, distance} =
+      input
+      |> parse_races()
+      |> Enum.reduce({"", ""}, fn {t, d}, {time, distance} ->
+        { time <> Integer.to_string(t), distance <> Integer.to_string(d) }
+      end)
+
+    time = String.to_integer(time)
+    distance = String.to_integer(distance)
+      IO.inspect({ time, distance})
+
+    get_min_press_time(time, distance)
+    |> IO.inspect()
+
+    # get_max_press(time, distance)
+    # |> IO.inspect()
+  end
+
+  defp get_min_press_time(total_time, max_distance),
+    do: get_min_press_time(0, total_time, max_distance)
+  defp get_min_press_time(press_time, total_time, max_distance) do
+    distance =
+      get_distance_for_button_press(press_time, total_time)
+    if distance > max_distance do
+      press_time
+    else
+      get_distance_for_button_press(press_time + 1, total_time)
+    end
+  end
+
+  defp get_possible_distances(total_time) do
+    Range.new(1, total_time - 1)
+    |> Enum.map(fn press_time ->
+      get_distance_for_button_press(press_time, total_time)
+    end)
+  end
+
+
+  defp get_distance_for_button_press(press_time, total_time) do
+    unpress_time = total_time - press_time
+    speed =
+      Range.new(1, press_time)
+      |> Enum.reduce(0, fn _timer, speed ->
+        speed + 1
+      end)
+
+    unpress_time * speed
+  end
+
+  defp parse_races(input) do
+    [times, distances] =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&find_numbers/1)
+
+    Enum.zip(times, distances)
+  end
+
+  defp find_numbers(line) do
+    ~r/\d+/
+    |> Regex.scan(line)
+    |> Enum.map(fn [number] ->
+      String.to_integer(number)
+    end)
   end
 end
--- test/advent_of_code/day_06_test.exs
+++ test/advent_of_code/day_06_test.exs
@@ -3,19 +3,16 @@ defmodule AdventOfCode.Day06Test do
 
   import AdventOfCode.Day06
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  Time:      7  15   30
+  Distance:  9  40  200
+  """
 
-    assert result
+  test "part1" do
+    288 = part1(@input)
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    71503 = part2(@input)
   end
 end
